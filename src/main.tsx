e63a140b7e8bed42a6c2cc6d339a0e6a7af2698c
Johannes Jahn <johannes.jahn@futurice.com>
2022-10-05 16:49:39 +0200

    Add first login flow

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { extendTheme } from "@chakra-ui/react"
 import Game from './game'
+import { setAxiosFactory, setBaseUrl } from './api/axios-client'
+import { QueryCache, QueryClient, QueryClientProvider } from 'react-query'
+import axios, { AxiosError } from 'axios'
 
 const theme = extendTheme({
     colors: {
@@ -18,8 +21,27 @@ const theme = extendTheme({
     },
 });
 
+const queryClient = new QueryClient();
+
+setBaseUrl('http://localhost:3333');
+setAxiosFactory(() => {
+
+    axios.interceptors.request.use((config: any) => {
+        const token = localStorage.getItem("accessToken");
+        if (token) {
+            config.headers['Authorization'] = 'Bearer ' + token
+        }
+        return config
+    });
+
+    return axios;
+})
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <ChakraProvider theme={theme}>
-        <Game />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+            <Game />
+        </ChakraProvider>
+    </QueryClientProvider>
 )
