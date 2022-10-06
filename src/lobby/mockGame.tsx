4c6e6a045b500ecc00ac66f0e61875e434e89224
Johannes Jahn <johannes.jahn@futurice.com>
2022-10-06 19:26:11 +0200

    only show active words

--- src/lobby/mockGame.tsx
+++ src/lobby/mockGame.tsx
@@ -11,8 +11,8 @@ const MockGame: FC<{ gameId: string }> = ({ gameId }) => {
     const { data } = GameControllerQuery.useGetGameStateQuery(gameId);
     const [currentWord, setCurrentWord] = useState("");
     const queryClient = useQueryClient();
+    const forceUpdate = useForceUpdate()
     useEffect(() => {
-
         const socket = io('http://localhost:3333/', {
             auth: {
                 token: localStorage.getItem("accessToken")
@@ -23,28 +23,32 @@ const MockGame: FC<{ gameId: string }> = ({ gameId }) => {
         });
 
         socket.on("message", (d: ActiveGameDataDto) => {
-            console.log(JSON.stringify(d));
-            if (d.status.gameFinished || d.status.gameStarted) {
+            if (d.status.gameStarted) {
                 queryClient.invalidateQueries(GameControllerQuery.getCurrentGameQueryKey());
+                queryClient.invalidateQueries(GameControllerQuery.getGameStateQueryKey(gameId));
             }
-            console.log(d);
         });
 
+        const updateInterval = setInterval(forceUpdate, 100)
         return () => {
+            clearInterval(updateInterval)
             socket.disconnect();
         }
-    }, [])
+    }, []);
 
     if (!data) {
         return <CircularProgress isIndeterminate />
     }
 
-   // console.log(data.game.startedTimestamp);
-  //  console.log(new Date().valueOf());
+
+    const currentTs = new Date().valueOf() - data.game.startedTimestamp;
 
     return <Container>
         <Box>
-            {data.wordsToBeSubmitted.map((w, idx) =>
+            {data.wordsToBeSubmitted.filter(
+                w => w.validFrom < currentTs &&
+                    w.validUntil > currentTs
+            ).map((w, idx) =>
                 <p key={idx}>{JSON.stringify(w)}</p>
             )}
         </Box>
@@ -60,6 +64,11 @@ const MockGame: FC<{ gameId: string }> = ({ gameId }) => {
                 }
             }} />
     </Container>
+}
+
+const useForceUpdate = () => {
+    const [value, setValue] = useState(0);
+    return () => setValue(value => value + 1);
 }
 
 export default MockGame;
