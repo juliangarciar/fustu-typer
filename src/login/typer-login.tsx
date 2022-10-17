5a44c7a548f40069fb5473fc72210cf6a8769253
Johannes Jahn <johannes.jahn@futurice.com>
2022-10-17 17:02:13 +0200

    Add google login

--- src/login/typer-login.tsx
+++ src/login/typer-login.tsx
@@ -1,7 +1,7 @@
 import { Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { useQueryClient } from "react-query";
-import { AuthControllerQuery, GameControllerQuery, LoginDto, RegisterDto, UsersControllerQuery } from "../api/axios-client";
+import { AuthControllerQuery, GameControllerQuery, GoogleTokenDto, LoginDto, RegisterDto, UsersControllerQuery } from "../api/axios-client";
 
 declare global {
     const google: typeof import('google-one-tap');
@@ -13,6 +13,8 @@ export const TyperLogin: FC = () => {
     const { data, refetch } = UsersControllerQuery.useMeQuery();
     const queryClient = useQueryClient();
 
+    const [googleRef, setGoogleRef] = useState<HTMLDivElement | null>(null);
+
     useEffect(() => {
         if (data?.email == null || data?.email == undefined) {
             if (!isOpen) onOpen();
@@ -22,69 +24,42 @@ export const TyperLogin: FC = () => {
     }, [data]);
 
     useEffect(() => {
+        // @ts-ignore
+        if (!window.google) return;
+        if (!googleRef) return;
         google.accounts.id.initialize({
             client_id: "1001740339512-uq8ofj3bap5qlu79hnkoci149ep7lq1b.apps.googleusercontent.com",
-            callback: (response) => { console.log(response); }
+            callback: (response) => {
+                AuthControllerQuery.Client.googleLogin(new GoogleTokenDto({ token: response.credential })).then(result => {
+                    localStorage.setItem("accessToken", result.accessToken);
+                    refetch();
+                });
+            }
         });
-        google.accounts.id.prompt();
-    }, []);
+        google.accounts.id.renderButton(
+            document.getElementById("googleButtonDiv")!,
+            { theme: "outline", size: "large", width: 336 }  // customization attributes
+        );
+        // @ts-ignore
+    }, [window.google, googleRef, data?.email]);
 
-    const handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const value = event.currentTarget.value;
-        const name = event.currentTarget.name;
-
-        setFormData((formData) => ({...formData, [name]: value}));
-    };
-
-    const login = async () => {
-        const result = await AuthControllerQuery.Client.login(new LoginDto({ ...formData }));
-        localStorage.setItem("accessToken", result.accessToken);
-        setFormData({ email: "", password: "" });
-        refetch();
-
-        queryClient.invalidateQueries(GameControllerQuery.getCurrentGameQueryKey());
-    };
-
-    const register = async () => {
-        await AuthControllerQuery.Client.register(new RegisterDto({ ...formData, name: "Noname" }));
-        await login();
-    };
 
     return (
-        <Modal  closeOnOverlayClick={false}
-                closeOnEsc={false}
-                isOpen={isOpen}
-                onClose={onClose}
-                isCentered={true}
-                size="sm"
-            >
+        <Modal closeOnOverlayClick={false}
+            closeOnEsc={false}
+            isOpen={isOpen}
+            onClose={onClose}
+            isCentered={true}
+            size="sm"
+        >
             <ModalOverlay />
             <ModalContent>
-                <ModalHeader>
+                <ModalHeader textAlign={"center"}>
                     Welcome to FuSTU Typer
                 </ModalHeader>
-                <ModalBody pb={4}>
-                    <FormControl>
-                        <FormLabel hidden={true}>E-Mail</FormLabel>
-                        <Input name="email" type="email" value={formData.email} placeholder="E-Mail" onChange={handleInputChange}/>
-                    </FormControl>
-                    <FormControl mt={4}>
-                        <FormLabel hidden={true}>Password</FormLabel>
-                        <Input name="password" type="password" value={formData.password} placeholder="Password" onChange={handleInputChange}/>
-                    </FormControl>
-                    
+                <ModalBody pb={4} ref={setGoogleRef}>
+                    <div id="googleButtonDiv" style={{ width: "100%" }}></div>
                 </ModalBody>
-                <ModalFooter>
-                    <Button colorScheme="teal" onClick={async () => {
-                        try {
-                            await login();
-                        } catch (e) {
-                            await register();
-                        }
-                    }}>
-                        Login/Register
-                    </Button>
-                </ModalFooter>
             </ModalContent>
         </Modal>
     );
