97df38954e4a5a1696a6f84a4fdbde180be5f0f9
Johannes Jahn <johannes.jahn@outlook.com>
2022-11-04 23:00:41 +0100

    Add postgame stats

--- src/menu/typer-game-ended.tsx
+++ src/menu/typer-game-ended.tsx
@@ -1,12 +1,15 @@
-import { Box, HStack } from "@chakra-ui/react";
-import { FC, useContext } from "react";
-import { useQueryClient } from "react-query";
-import { GameControllerQuery, UsersControllerQuery } from "../api/axios-client";
-import TyperContainer from "../common-components/typer-container";
-import { TyperLoading } from "../common-components/typer-loading";
-import { TyperMenuLayout } from "../common-components/typer-menu-layout";
-import { TyperPlayerCard } from "../common-components/typer-player-card";
-import { GameStateContext, GAME_STATE } from "../common-hooks/typer-gamestate-context";
+import { Box, HStack } from '@chakra-ui/react';
+import { FC, useContext } from 'react';
+import { useQueryClient } from 'react-query';
+import { GameControllerQuery, UsersControllerQuery } from '../api/axios-client';
+import TyperContainer from '../common-components/typer-container';
+import { TyperLoading } from '../common-components/typer-loading';
+import { TyperMenuLayout } from '../common-components/typer-menu-layout';
+import { TyperPlayerCard } from '../common-components/typer-player-card';
+import {
+  GameStateContext,
+  GAME_STATE,
+} from '../common-hooks/typer-gamestate-context';
 
 export const TyperGameEnded: FC = () => {
   const { data: lastGame } = GameControllerQuery.useGetLastGameQuery();
@@ -20,39 +23,45 @@ export const TyperGameEnded: FC = () => {
   };
 
   if (!lastGame) return <TyperLoading />;
-  
+
   return (
     <TyperContainer>
       <TyperMenuLayout
-        heading={"Result"}
+        heading={'Result'}
         cancelButton={{
-          buttonName: "Back to menu",
+          buttonName: 'Back to menu',
           buttonAction: () => handleBackToMenu(),
         }}
         acceptButton={undefined}
       >
         <HStack h="100%" mx="2.5%" spacing="5%">
-          {
-            lastGame.participants.map((participant, index) => {
-              return (
-                
-                <Box key={index} w="47.5%" h="80%" bg={lastGame?.winner.id === currentUser?.id ? "yellow.100" : "red.200"}>
-                  <TyperPlayerCard
-                    playerName={participant.name}
-                    playerEmail={participant.email}
-                    playerImg={participant.avatar}
-                    playerElo={participant.rating}
-                    playerGames={participant.gamesPlayed}
-                    playerRankedGames={participant.rankedGamesPlayed}
-                    playerCorrectWords={0}
-                    playerIncorrectWords={0}
-                  />
-                </Box>
-              );
-            })
-          }
+          {lastGame.postGameUsers.map((participant, index) => {
+            return (
+              <Box
+                key={index}
+                w="47.5%"
+                h="80%"
+                bg={
+                  lastGame?.winner.id === currentUser?.id
+                    ? 'yellow.100'
+                    : 'red.200'
+                }
+              >
+                <TyperPlayerCard
+                  playerName={participant.userInfo.name}
+                  playerEmail={participant.userInfo.email}
+                  playerImg={participant.userInfo.avatar}
+                  playerElo={participant.userInfo.rating}
+                  playerGames={participant.userInfo.gamesPlayed}
+                  playerRankedGames={participant.userInfo.rankedGamesPlayed}
+                  playerCorrectWords={participant.correctSubmissions}
+                  playerIncorrectWords={participant.wrongSubmissions}
+                />
+              </Box>
+            );
+          })}
         </HStack>
       </TyperMenuLayout>
     </TyperContainer>
   );
-}
+};
